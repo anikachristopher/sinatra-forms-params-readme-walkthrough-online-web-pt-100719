1e113f133e12847e2f77f579eb6c8383638c00c8
Anika Christopher <anika.mchristopher@gmail.com>
2020-01-31 00:46:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,8 +4,15 @@ class App < Sinatra::Base
 
   get '/food_form' do
     erb :food_form
+    params = {
+  :name => "Sam",
+  :favorite_food => "Green Eggs and Ham"
+}
   end
 
   # Add your post route and action below
-
+  post '/food' do
+    # params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
